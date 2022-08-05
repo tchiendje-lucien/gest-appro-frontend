fe1cddc5b6b59e71653cc525e0026f71cc47671d
TCHIENDJE Lucien <tchiendjelucien@gmail.com>
2022-08-05 12:55:04 +0100

    Mon premier commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SettingsComponent } from './settings/settings.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SettingsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
